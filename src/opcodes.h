f83964da7205d225dd216c29b0e11cafbff0e0d7
ggponti74 <ggponti74@hotmail.com>
2026-09-21 11:18:42 -0400

    Add opcode lookup and emit function declarations

--- src/opcodes.h
+++ src/opcodes.h
@@ -1,1 +1,14 @@
+#ifndef OPCODES_H
+#define OPCODES_H
 
+#include <stddef.h>
+#include <stdint.h>
+#include "cpu68k.h"
+
+// Lookup function: find an opcode entry by mnemonic
+const OpcodeEntry* lookup_opcode(const char *mnemonic);
+
+// Emit function: write opcode bytes to output stream
+void emit_opcode(FILE *out, const OpcodeEntry *entry);
+
+#endif // OPCODES_H
