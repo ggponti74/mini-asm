2bf92bb310fe1a9a3e66711e82ee79d8004675fd
ggponti74 <ggponti74@hotmail.com>
2026-09-21 11:23:18 -0400

    Add header guard and parse_line function declaration
    Added header guard and function declaration for parse_line.

--- src/parser.h
+++ src/parser.h
@@ -1,1 +1,12 @@
+#ifndef PARSER_H
+#define PARSER_H
 
+#include <stddef.h>
+#include "cpu68k.h"
+
+// Parse a single line of source code.
+// Returns an OpcodeEntry pointer if valid, NULL if error.
+// line_num is used for error reporting.
+const OpcodeEntry* parse_line(const char *line, size_t line_num);
+
+#endif // PARSER_H
