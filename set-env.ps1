ce751d52f5cecf9f03b762f1de58b3f66896f5fc
ggponti74 <ggponti74@hotmail.com>
2026-09-24 16:15:03 -0400

    Environment Setup

---
+++ set-env.ps1
@@ -0,0 +1,5 @@
+$ENV:Path += ";C:\Users\Puser.CFL2024-P-03\OneDrive\DevEnv\Git\bin"
+$ENV:Path += ";C:\Users\Puser.CFL2024-P-03\OneDrive\DevEnv\mingw64\bin"
+
+git config --global user.email "ggponti74@hotmail.com"
+git config --global user.name "ggponti74"
